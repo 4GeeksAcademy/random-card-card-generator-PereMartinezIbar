305f9633bf5d5cbb86baaceb79468bfe2cbe2b0a
PeritxCreate <143961072+PeritxCreate@users.noreply.github.com>
2023-11-17 19:12:17 +0000

    carta se genera

--- src/app.js
+++ src/app.js
@@ -5,7 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function card() {
+  let numbers = Math.floor(Math.random() * 13);
+  let suits = Math.floor(Math.random() * 4);
+  let suitsTypes = ["♦", "♥", "♠", "♣"];
+  let numbersType = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  if (suits === 0 || suits === 1) {
+    document.getElementById("card").style.color = "red";
+  }
+  document.getElementById("numbers").innerHTML = numbersType[numbers];
+  document.getElementById("suits1").innerHTML = suitsTypes[suits];
+  document.getElementById("suits2").innerHTML = suitsTypes[suits];
 };
